260e9e24c10af04b2d655c6f68f152337073c40f
Abhinav <17122000abhinav@gmail.com>
2022-06-28 09:10:12 +0530

    Initital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import PreNavbar from './Components/PreNavBar';
+import NavBar from './Components/NavBar';
+import Slider from './Components/Slider';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import data from "./data/data.json"
+import Offers from './Components/Offers';
+import Heading from './Components/Heading';
+import StarProduct from './Components/StarProduct';
+import HotAccessoriesMenu from './Components/HotMenu';
+import HotAccessories from './Components/HotAccessories';
+import ProductReviews from './Components/ProductReviews';
+import Videos from './Components/Videos';
+import Banner from './Components/Banner';
+import Footer from './Components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <PreNavbar />
+      <NavBar />
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading text={"STAR PRODUCTS"} />
+      <StarProduct products={data.starProduct} />
+      <Heading text={"HOT ACCESSORIES"} />
+      <HotAccessoriesMenu />
+      <Routes>
+        <Route exact path="/music" element={<HotAccessories cover={data.hotAccessoriesCover.music} catagory={data.hotAccessories.music} />} />
+        <Route exact path="/smartDevice" element={<HotAccessories cover={data.hotAccessoriesCover.smartDevice} catagory={data.hotAccessories.smartDevice} />} />
+        <Route exact path="/home" element={<HotAccessories cover={data.hotAccessoriesCover.home} catagory={data.hotAccessories.home} />} />
+        <Route exact path="/lifeStyle" element={<HotAccessories cover={data.hotAccessoriesCover.lifeStyle} catagory={data.hotAccessories.lifeStyle} />} />
+        <Route exact path="/mobileAccessories" element={<HotAccessories cover={data.hotAccessoriesCover.mobileAccessories} catagory={data.hotAccessories.mobileAccessories} />} />
+      </Routes>
+      <Heading text={"PRODUCT REVIEWS"} />
+      <ProductReviews products={data.productReviews} />
+      <Heading text={"VIDEOS"} />
+      <Videos Videos={data.videos} />
+      <Heading text={"IN THE PRESS"} />
+      <Banner banner={data.banner.end} />
+      <Footer />
+    </Router>);
 }
 
 export default App;
